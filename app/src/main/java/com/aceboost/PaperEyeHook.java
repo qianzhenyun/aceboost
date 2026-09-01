126d18ab3343e59e2656a2a3534102a1c699ca87
qianzhenyun <151329173+qianzhenyun@users.noreply.github.com>
2026-09-01 17:05:31 +0800

    fix: PaperEyeHook layer type paint

--- app/src/main/java/com/aceboost/PaperEyeHook.java
+++ app/src/main/java/com/aceboost/PaperEyeHook.java
@@ -2,6 +2,7 @@ package com.aceboost;
 
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
 import android.view.View;
 import de.robv.android.xposed.*;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
@@ -18,7 +19,8 @@ public class PaperEyeHook {
             LogUtil.log("PaperEyeHook init package=" + lp.packageName + " enabled=" + enabled);
             if (!enabled) return;
 
-            final ColorMatrixColorFilter filter = new ColorMatrixColorFilter(GRAYSCALE);
+            final Paint paint = new Paint();
+            paint.setColorFilter(new ColorMatrixColorFilter(GRAYSCALE));
 
             XposedHelpers.findAndHookMethod("android.view.ViewRootImpl", lp.classLoader,
                 "setView", View.class, int.class, int.class, new XC_MethodHook() {
@@ -27,7 +29,7 @@ public class PaperEyeHook {
                         try {
                             View root = (View) param.args[0];
                             if (root != null) {
-                                root.setLayerType(View.LAYER_TYPE_HARDWARE, filter);
+                                root.setLayerType(View.LAYER_TYPE_HARDWARE, paint);
                             }
                         } catch (Throwable ignored) {}
                     }
