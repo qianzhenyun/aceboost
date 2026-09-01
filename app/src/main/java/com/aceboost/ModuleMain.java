90d61d614aee6e84ecfb98761690d5f54d377e2f
qianzhenyun <151329173+qianzhenyun@users.noreply.github.com>
2026-09-01 12:59:26 +0800

    fix liquid glass switch

--- app/src/main/java/com/aceboost/ModuleMain.java
+++ app/src/main/java/com/aceboost/ModuleMain.java
@@ -8,11 +8,22 @@ public class ModuleMain implements IXposedHookLoadPackage {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lp) {
         try {
-            if (lp.packageName.equals("android")) {
+            String pkg = lp.packageName;
+            if (pkg.equals("com.tencent.mm") || pkg.equals("com.tencent.mobileqq")) {
+                if (PrefsReader.getBool("liquid_glass_enable", false)) {
+                    HostApp app = HostApp.forPackage(pkg);
+                    if (app != null) {
+                        LiquidGlassModule.setApp(app);
+                        LiquidGlassModule.install(lp.classLoader);
+                    }
+                }
+            }
+
+            if (pkg.equals("android")) {
                 AudioHook.hook(lp);
                 VibrationHook.hook(lp);
                 AntiDetectionHook.hook(lp);
-            } else if (lp.packageName.equals("com.android.mms") || lp.packageName.equals("com.google.android.apps.messaging")) {
+            } else if (pkg.equals("com.android.mms") || pkg.equals("com.google.android.apps.messaging")) {
                 VerificationCodeHook.hook(lp);
             }
             ScreenBurnHook.hook(lp);
